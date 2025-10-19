032d159496850ff677a6237e3d599db993c2b2d5
Max-dot668 <maxiyouit@gmail.com>
2025-10-19 00:59:28 -0700

    Complete JavaScript variables and operators assignment

---
+++ assignment/script.js
@@ -0,0 +1,24 @@
+// 1. Add 2 numbers together
+console.log(23 + 97);
+
+// 2. Add 6 different numbers together 
+console.log(23 + 54 + 3 + 65 + 75 + 50);
+
+// 3. Log an expression 
+console.log(4 + 6 + 9) / 77;
+
+// 4. VARIABLES
+
+let a = 10;
+console.log(a);
+
+a = 4;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
